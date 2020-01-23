0b827997714930da0f59ef9e71e6fe5eb1479bf4
blandine-dev <viry.blandine@gmail.com>
2020-01-23 17:08:01 +0100

    je sais pas

--- imports/pages/Signin.js
+++ imports/pages/Signin.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import {Form, Button, Container} from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { withTracker } from 'meteor/react-meteor-data'
 
 
 function Signin(props){
@@ -11,23 +12,35 @@ function Signin(props){
         Meteor.loginWithPassword(email, password)
     }
 
+    const logout = () => Meteor.logout()
+
 
     return(
             <Container>
                 <h1>Connectez-vous!</h1>
-                
-                <Form onSubmit={signin}>
-                    <Form.Input value={email} onChange={(e, {value}) => setEmail(value)} label="Email" type="email" placeholder="ex: toto@yopmail.com" />
-                    <Form.Input value={password} onChange={(e, {value}) => setPassword(value)} label="Mot de passe" type="password" />
-                    <Button disabled={!email || !password} color="blue">M'inscrire</Button>
-                    <Link to='/signup'>
-                        <Button size="mini">Créer un compte</Button>
-                    </Link>
-                </Form>
-
+                {!props.current_user ?
+                    <Form onSubmit={signin}>
+                        <Form.Input value={email} onChange={(e, {value}) => setEmail(value)} label="Email" type="email" placeholder="ex: toto@yopmail.com" />
+                        <Form.Input value={password} onChange={(e, {value}) => setPassword(value)} label="Mot de passe" type="password" />
+                        <Button disabled={!email || !password} color="blue">M'inscrire</Button>
+                        <Link to='/signup'>
+                            <Button size="mini">Créer un compte</Button>
+                        </Link>
+                    </Form>
+                :
+                    <div>
+                    <p>Vous êtes déjà connecté</p>
+                    <Button onClick={logout} color="red">Se déconnecter</Button>
+                    </div>
+                }
             </Container>
     )
 }
 
 
-export default Signin
+export default withTracker(()=>{
+    const current_user = Meteor.user()
+    return {
+        current_user
+    }
+})(Signin)
