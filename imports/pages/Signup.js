093b2ef034ab30f34712c5e49809f07550b188f8
blandine-dev <viry.blandine@gmail.com>
2020-01-23 12:47:41 +0100

    Added signup form semantic ui

--- imports/pages/Signup.js
+++ imports/pages/Signup.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import {Form, Button, Container} from 'semantic-ui-react'
+
 function Signup(props){
     return(
-        <p>Signup</p>
+            <Container>
+            <h1>Inscrivez-vous!</h1>
+            <Form>
+                <Form.Input label="Email" type="email" placeholder="ex: toto@yopmail.com" />
+                <Form.Input label="Mot de passe" type="password" />
+                <Button color="blue">M'inscrire</Button>
+            </Form>
+            </Container>
     )
 }
 
